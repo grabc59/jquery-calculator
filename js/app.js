16c61eaef28f9b2cecbb545bf17fdf87892d6cd6
Christopher Grabski <grabc59@gmail.com>
2016-10-20 15:12:42 -0600

    testing duplicate operators, and moving clear to a separate function

---
+++ js/app.js
@@ -0,0 +1,67 @@
+'use strict';
+$( document ).ready(function() {
+  var $screen = $('#screen');
+  var calcInput = "";
+  $screen.text(calcInput);
+
+//////////////////////////////////////
+////////// BUTTON LISTENER TEST
+//////////////////////////////////////
+$('#buttons-container').on('click', function(event) {
+  var $target = $(event.target);
+  if ($target.text() === "C") {
+      clearScreen();
+  } else {
+    switch ($target.text()) {
+      case "÷":
+        operatorCheck($target);
+        calcInput += "/";
+        break;
+      case "x":
+        calcInput += "*";
+        break;
+      case "+":
+        calcInput += "+";
+        break;
+      case "-":
+        calcInput += "-";
+        break;
+      case "=":
+        calcInput = eval(calcInput);
+        break;
+      default:
+        calcInput += $target.text(); // add the current target text to the stored calcInput variable. Intended to be numbers.
+    }
+  }
+    // console.log(calcInput);
+    $screen.text(calcInput); // every time a button is clicked, update the screen with all input
+
+
+});
+
+//////////////////////////////////////
+////////// SCREEN TEST
+//////////////////////////////////////
+// $('#screen').text('hello');
+
+//////////////////////////////////////
+////////// CLEARSCREEN
+//////////////////////////////////////
+function clearScreen() {
+  calcInput = "";
+}
+
+
+//////////////////////////////////////
+////////// CHECK FOR MULTIPLE OPERATORS
+//////////////////////////////////////
+  function operatorCheck($target) {
+    var operators = ["+", "-", "*", "/"];
+    var lastChar = calcInput.slice(calcInput.length-1)
+    for (var i = 0; i < operators.length; i++) {
+      if (lastChar === operators[i]) {
+        calcInput = "Error";
+      }
+    }
+  }
+});
