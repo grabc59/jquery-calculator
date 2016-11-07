6ffa66cc5b0e134960b159a5005b9be62396c90b
Christopher Grabski <grabc59@gmail.com>
2016-11-06 19:26:18 -0700

    prevent clicking between buttons adding all

--- js/app.js
+++ js/app.js
@@ -5,41 +5,46 @@ $(document).ready(function() {
     var currentIsOperator = false;
     var prevWasOperator = false;
 
-
     //////////////////////////////////////
     ////////// BUTTON LISTENER TEST
     //////////////////////////////////////
-    $('#buttons-container').on('click', function(event) {
-        var $target = $(event.target);
-        currentIsOperator = $target.hasClass("operator");
-        if ($target.text() === "C") {
-            calcInput = ""
-        } else if (currentIsOperator && prevWasOperator) {
-            calcInput = "Error";
-            $screen.text(calcInput);
-        } else {
-            // at this point the entry will be added to the calculator input. to prepare for the next input, this input becomes the 'previous' input (the value of whether the 'current' was an operator passes to 'previous')
-            prevWasOperator = currentIsOperator;
-            switch ($target.text()) {
-                case "÷":
-                    calcInput += "/";
-                    break;
-                case "x":
-                    calcInput += "*";
-                    break;
-                case "+":
-                    calcInput += "+";
-                    break;
-                case "-":
-                    calcInput += "-";
-                    break;
-                case "=":
-                    calcInput = eval(calcInput);
-                    // don't treat 'equals' as an operator, set it to false. this allows operations to be done on a calculated result.
-                    prevWasOperator = false;
-                    break;
-                default:
-                    calcInput += $target.text();
+    $('.buttons').on('click', function(event) {
+        // don't register clicks to the space between buttons (where target is the actual buttons container)
+        // console.log(event.target, event.currentTarget);
+        if (event.target !== event.currentTarget) {
+          var $target = $(event.target);
+          currentIsOperator = $target.hasClass("operator");
+            if ($target.text() === "C") {
+                calcInput = "";
+            } else if (calcInput === "Error") {
+                return;
+            } else if (currentIsOperator && prevWasOperator) {
+                calcInput = "Error";
+                $screen.text(calcInput);
+            } else {
+                // at this point the entry will be added to the calculator input. to prepare for the next input, this input becomes the 'previous' input (the value of whether the 'current' was an operator passes to 'previous')
+                prevWasOperator = currentIsOperator;
+                switch ($target.text()) {
+                    case "÷":
+                        calcInput += "/";
+                        break;
+                    case "x":
+                        calcInput += "*";
+                        break;
+                    case "+":
+                        calcInput += "+";
+                        break;
+                    case "-":
+                        calcInput += "-";
+                        break;
+                    case "=":
+                        calcInput = eval(calcInput);
+                        // don't treat 'equals' as an operator, set it to false. this allows operations to be done on a calculated result.
+                        prevWasOperator = false;
+                        break;
+                    default:
+                        calcInput += $target.text();
+                }
             }
         }
         $screen.text(calcInput); // every time a button is clicked, update the screen with all input
